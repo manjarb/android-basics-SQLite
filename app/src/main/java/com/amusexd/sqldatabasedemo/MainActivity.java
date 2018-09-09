cbe5e04a5463e8c84c1d79e24bccda7bf3ebc5d5
Varis Darasirikul <varis.dara@gmail.com>
2018-09-10 01:34:39 +0800

    Update more sql command

--- app/src/main/java/com/amusexd/sqldatabasedemo/MainActivity.java
+++ app/src/main/java/com/amusexd/sqldatabasedemo/MainActivity.java
@@ -16,18 +16,25 @@ public class MainActivity extends AppCompatActivity {
         try {
             SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
 
-            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
-            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Rob', 34)");
-            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Tommy', 4)");
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3), id INTEGER PRIMARY KEY)");
+            // myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Kirsten', 34)");
+            // myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Ralphie', 4)");
+            // myDatabase.execSQL("DELETE FROM users WHERE name = 'Kirsten'");
+            // myDatabase.execSQL("DELETE FROM users WHERE id = 1");
+            // myDatabase.execSQL("UPDATE users SET age = 2 WHERE name = 'Ralphie' LIMIT 1");
 
-            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+            // Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+            Cursor c = myDatabase.rawQuery("SELECT * FROM users WHERE age < 50 AND name = 'Kirsten'", null);
+
             int nameIndex = c.getColumnIndex("name");
             int ageIndex = c.getColumnIndex("age");
+            int idIndex = c.getColumnIndex("id");
 
             c.moveToFirst();
             while (c != null) {
-                Log.i("name", c.getString(nameIndex));
-                Log.i("age", Integer.toString(c.getInt(ageIndex)));
+                Log.i("Results - name", c.getString(nameIndex));
+                Log.i("Results - age", Integer.toString(c.getInt(ageIndex)));
+                Log.i("Results - id", Integer.toString(c.getInt(idIndex)));
 
                 c.moveToNext();
             }
